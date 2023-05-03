ec960b233a1fdfdc8029269708753648d52edba5
Yongli <yl.low91@gmail.com>
2023-05-03 14:44:05 +0800

    first commit

---
+++ server/src/main/java/iss/csf/day37workshop/Day37WorkshopApplication.java
@@ -0,0 +1,13 @@
+package iss.csf.day37workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37WorkshopApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/iss/csf/day37workshop/Day37WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.csf.day37workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
